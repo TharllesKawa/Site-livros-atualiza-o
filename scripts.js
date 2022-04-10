45f2962bbc7a6d90a47c77d647af273c8a0eb70b
TharllesKawa <tharlleskawa22@gmail.com>
2022-04-10 11:32:20 -0300

    atualizando-email

---
+++ scripts.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.textos-meio', {duration: 1000});
+
+sr.reveal('.imagem-meio', {
+    rotate: { x: 0, y: 80, z: 0},
+    duration: 2000
+})
+
+sr.reveal('.container-meio', {duration: 1000});
+
+sr.reveal('.texto-livro', {
+    rotate: { x: 50, y: 50, z: 0},
+    duration: 2000,
+});
+
+sr.reveal('.cards-livros', {duration: 1000});
